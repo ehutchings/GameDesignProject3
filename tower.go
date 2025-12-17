ef728456ebdde9ea99d298436c8c8e6481359f8f
ehutchings <edanhutchings@gmail.com>
2025-12-16 20:46:33 -0500

    added fire projectile method to tower struct, currently only supports crossbow

--- tower.go
+++ tower.go
@@ -37,6 +37,7 @@ func (tower *tower) Draw(drawOps *ebiten.DrawImageOptions, screen *ebiten.Image)
 }
 
 func (tower *tower) getTarget(enemies []*enemy) {
+	tower.targetEnemy = nil
 	highestDistanceTravelled := 0
 	for _, currentEnemy := range enemies {
 		if tower.rangeCollider.IsIntersecting(currentEnemy.collider) ||
@@ -63,5 +64,22 @@ func newCrossbowTower(x, y int) *tower {
 		currentFrame:  0,
 		frameLength:   3,
 		rangeCollider: resolv.NewCircle(float64(x-TILE_WIDTH/2), float64(y-TILE_HEIGHT/2), float64(radius)),
+	}
+}
+
+func (tower *tower) fireProjectile(projManager *projectileManager) {
+	if tower.typeOfTower == crossbow {
+		sprite := LoadEmbeddedImage("Projectiles", "crossbowBolt.png")
+		newProjectile := projectile{
+			x:               tower.x,
+			y:               tower.y,
+			sprite:          sprite,
+			xDirection:      1,
+			yDirection:      1,
+			inheritedDamage: tower.baseDamage,
+			speed:           8,
+			effect:          damageOnly,
+		}
+		projManager.projectiles = append(projManager.projectiles, &newProjectile)
 	}
 }
